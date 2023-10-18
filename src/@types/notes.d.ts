49824553f1aad5cf03f7931cb4772ac8a4861360
Ian Cenry <iancenry@gmail.com>
2023-10-19 01:08:29 +0300

    chore: update types and packages

--- src/@types/notes.d.ts
+++ src/@types/notes.d.ts
@@ -1,7 +1,9 @@
+// Added id to data from form; actual note data structure
 export type Note = {
   id: string
 } & NoteData
 
+// data from the form
 export type NoteData = {
   title: string
   markdown: string
@@ -13,6 +15,18 @@ export type Tag = {
   label: string
 }
 
-export type NoteFormProps = {
-  onSubmit: (data: NoteData) => void
+// store the id of the tag instead of the tag so that changes are propagated without editing every single note
+export type RawNote = {
+  id: string
+} & RawNoteData
+
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+
+export type NewNoteProps = {
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
